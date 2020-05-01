5b40413e6751ac1ee5e812d43017d7405cd5448c
VICKY <dubey.rajesh95@gmail.com>
2020-05-01 22:10:51 +0530

    Update

--- Src/main.c
+++ Src/main.c
@@ -10,10 +10,55 @@
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
+/*
+ * 002LEDWithBtn.c
+ *
+ *  Created on: 14-Mar-2020
+ *      Author: vicky
+ */
 
 #include"main.h"
 
-int main(void)
+#define HIGH 					1
+#define BTN_PRESSED 			HIGH
+
+void delay(void)
 {
+	for(uint32_t i = 0; i < 500000; i++);
+}
+
+int main()
+{
+	GPIO_Handle_t GpioLed, GpioBtn;
+	GpioLed.pGPIOx = GPIOG;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber 		= GPIO_PIN_NO_13;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUTPUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed		= GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType 		= GPIO_OUT_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_NO_PUPD;
+
+	GPIO_PCLK_Control(GPIOG, ENABLE);
+
+	GPIO_Init(&GpioLed);
+
+	GpioBtn.pGPIOx = GPIOA;
+	GpioBtn.GPIO_PinConfig.GPIO_PinNumber 		= GPIO_PIN_NO_0;
+	GpioBtn.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_INPUT;
+	GpioBtn.GPIO_PinConfig.GPIO_PinSpeed		= GPIO_SPEED_FAST;
+	GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PIN_PD;
+
+	GPIO_PCLK_Control(GPIOA, ENABLE);
+
+	GPIO_Init(&GpioBtn);
+
+	while(1)
+	{
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOG, GPIO_PIN_NO_13);
+		}
+	}
 	return 0;
 }
+
